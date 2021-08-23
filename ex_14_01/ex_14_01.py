4aa4a900ffb96659bbbbe403eb453d4db07ed769
wilsonnwc <wilson.nwc@gmail.com>
2021-08-23 23:32:14 +0800

    Adding self notes for ex_14_01

--- ex_14_01/ex_14_01.py
+++ ex_14_01/ex_14_01.py
@@ -1,7 +1,8 @@
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
-# http://py4e-data.dr-chuck.net/comments_42.xml
+
+# test link: http://py4e-data.dr-chuck.net/comments_42.xml
 
 url = input('Enter location: ')
 print("Retrieving", url)
@@ -10,13 +11,15 @@ print("Retrieved:", len(data), "characters")
 tree = ET.fromstring(data)
 sum = 0
 datacount = tree.findall('.//comment')
+#reminder: to count len of data means to count the sets of grandchild here
 print("Count:", len(datacount))
 
+
 for content in datacount:
+    #".text" is required to get the element shown; "get('hide')" is for hide elements
     name = content.find('name').text
     count = content.find("count").text
     #print(name, count)
     sum = sum + int(count)
 
-print("Sum:", sum)
 print("Sum:", sum)
